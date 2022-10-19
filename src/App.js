106dad97f2cc418bfb759e15c7ea236bb0da47be
Tymivv <if210683tvn@gmail.com>
2022-10-19 18:43:37 +0300

    add_materials

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import UserRoutes from "./UserRoutes";
 import Footer from "./modules/Footer/Footer";
+import Materials from "./modules/Materials/"
 import "./index.scss";
 
 function App() {
   return (
     <>
       <UserRoutes />
+      <Materials />
       <Footer/>
     </>
     
