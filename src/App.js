1abf88345b76c05c4e23d98644244a5afa67df0e
Romalyy <1303roma01@gmail.com>
2022-10-23 10:22:08 +0300

    add_testingbutton

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux/es/exports";
 import { getUser } from "./redux/auth/authOperations";
 
+import { useMediaPredicate } from "react-media-hook";
+
 import Header from "./modules/Header";
 import Footer from "./modules/Footer";
 import UserRoutes from "./UserRoutes";
 
 import "./index.scss";
 
-function App() {
+function App({ onClick }) {
+  
+  const isMobile = useMediaPredicate("(max-width: 767px)");
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -17,8 +22,8 @@ function App() {
 
   return (
     <>
-      <Header/>
-      
+      <Header />
+
       <UserRoutes />
       
       <Footer />
