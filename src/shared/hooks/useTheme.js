9d58e11eea8d852e443d49b731a22c78cade7db6
Romalyy <1303roma01@gmail.com>
2022-10-27 22:50:00 +0300

    Update useTheme.js

--- src/shared/hooks/useTheme.js
+++ src/shared/hooks/useTheme.js
@@ -1,17 +1,7 @@
 import { useState, useLayoutEffect } from "react";
 
-// export const useTheme = () => {
-//     const [theme, setTheme] = useState('light');
-
-//     useLayoutEffect(() => {
-//         document.documentElement.setAttribute('data-theme', theme)
-//     }, [theme])
-//     return { theme, setTheme };
-// };
-
-
 const isDarkTheme = window?.matchMedia('(prefers-color-scheme: dark)').matches
-const defaultTheme = isDarkTheme ? 'dark' : 'light'
+const defaultTheme = !isDarkTheme ? 'light'  : 'dark'
 
 export const useTheme = () => {
   const [theme, setTheme] = useState(
